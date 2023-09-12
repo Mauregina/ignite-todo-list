5e73ea1d7ad287a8133a2da97fc9660da5d0fd91
Mauregina <mauraregina@gmail.com>
2023-09-12 11:52:03 -0400

    finish stylization

--- src/components/Tasklist.tsx
+++ src/components/Tasklist.tsx
@@ -1,5 +1,6 @@
 import {Trash} from 'phosphor-react';
 import styles from './Tasklist.module.css';
+import clipboard from '../assets/clipboard.svg';
 
 export function Tasklist() {
     return (
@@ -8,15 +9,24 @@ export function Tasklist() {
                 <div className={styles.taskCreated}>Tarefas criadas<span>5</span></div>
                 <div className={styles.taskDone}>Concluídas<span>5 de 10</span></div>
             </div>
+
+            <div className={styles.noData}>
+                <img src={clipboard}></img>
+                <div className={styles.msg}>
+                    <strong>Você ainda não tem tarefas cadastradas</strong>
+                    <span>Crie tarefas e organize seus itens a fazer</span>
+                </div>
+            </div>
+            
             <div className={styles.listItem}>
                 <input type='checkbox' />
                 <div>Coluna 1 Coluna 1Coluna 1Coluna 1Coluna 1Coluna 1Coluna 1Coluna 1Coluna 1Coluna 1</div>
-                <Trash />
+                <Trash size={16}/>
             </div>
             <div className={styles.listItem}>
                 <input type='checkbox' />
                 <div>Coluna 1 Coluna 1Coluna 1Coluna 1Coluna 1Coluna 1Coluna 1Coluna 1Coluna 1Coluna 1</div>
-                <Trash />
+                <Trash size={16}/>
             </div>
         </div>
     )
